bf67f349ba9b99c800f0018ce867f525fe0d96a5
Seungwoo, Joo <thisis.joos@gmail.com>
2024-02-01 19:53:16 +0900

    init setting

---
+++ divelog/src/main/java/com/fastcampus/joos/divelog/DivelogApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.joos.divelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivelogApplication.class, args);
+	}
+
+}
---
+++ divelog/src/test/java/com/fastcampus/joos/divelog/DivelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.joos.divelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
